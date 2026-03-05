4e803957737d3f2a1c8f547c485384d0c3bc8880
Mainakdey1 <mainakde02@gmail.com>
2026-03-05 19:11:05 +0530

    Added base post methods

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = FastAPI()
+@app.post("/base")
+def base():
+    return {'message': 'hello world'}
+
+if __name__ == "__main__":
+    port = int(os.getenv('PORT'))
+    host = os.getenv('HOST')
+    uvicorn.run(app, host=host, port=port)
